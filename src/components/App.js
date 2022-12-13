309302462bff38875dd47c5b5ea5f8b5e62305b1
Samuel Majerus <ladolego@gmail.com>
2022-12-13 09:56:20 -0800

    Adds CSS functionality (via JS) that changes the background- and text-colors on the page each time the  'Toggle Theme' button is clicked.    [End of code-along work for Lsn 9 (see link).] [https://www.learnhowtoprogram.com/react/react-with-apis/help-queue-creating-a-context-and-provider]

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import { ThemeContext, themes } from "./../context/theme-context";
 function App(){ 
   const [theme, setTheme] = useState(themes.light); 
 
+  //Set values for 'background color' and 'text color' to be the current theme. 
+  document.body.style.backgroundColor = theme.backgroundColor;  //New code! 
+  document.body.style.color = theme.textColor;  //New code! 
+
   function toggleTheme() {
     setTheme(theme => 
       theme.textColor === "AntiqueWhite" ? themes.light : themes.dark
@@ -16,7 +20,7 @@ function App(){
   return ( 
     <ThemeContext.Provider value={theme}> 
       <Header />
-      <ToggleTheme toggleTheme={toggleTheme}/> {/* new code! */}
+      <ToggleTheme toggleTheme={toggleTheme}/> 
       <TicketControl />
     </ThemeContext.Provider>
   );
